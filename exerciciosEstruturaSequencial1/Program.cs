cd1e7ef2c25450f904b9232fc1dbccaec7759ee2
Felipe Vido <contatofelipevido@gmail.com>
2023-12-25 15:46:57 -0300

    Exercícios 1 a 6
    Exercícios 1 solucionado.

--- exerciciosEstruturaSequencial1/Program.cs
+++ exerciciosEstruturaSequencial1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+System.Console.WriteLine("Informe dois valores inteiros separados por espaço:");
+
+string[] numbers = Console.ReadLine().Split(' ');
+int x = int.Parse(numbers[0]);
+int y = int.Parse(numbers[1]);
+
+double soma = x + y;
+System.Console.WriteLine("A soma dos valores " + x + " + " + y + " são iguais a " + soma.ToString("F1", CultureInfo.InvariantCulture));
