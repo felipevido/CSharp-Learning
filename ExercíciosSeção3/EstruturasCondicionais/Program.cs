c7893429ddffd7d8ba3d5c7508a7f9b21fa52d65
Felipe Vido <contatofelipevido@gmail.com>
2023-12-28 19:50:51 -0300

    Estruturas Condicionais
    Exercícios 1 solucionado.

---
+++ ExercíciosSeção3/EstruturasCondicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Globalization;
+
+int number = 0;
+char loop;
+
+do
+{
+    System.Console.WriteLine("Informe um número inteiro:");
+    number = int.Parse(Console.ReadLine());
+    
+    if (number < 0)
+    {
+        System.Console.WriteLine("O número digitado é negativo.");
+    }
+    else
+    {
+        System.Console.WriteLine("O número digitado é positivo.");
+    }
+    System.Console.WriteLine("'S' para refazer e 'N' para finalizar:");
+    loop = char.Parse(Console.ReadLine());
+} while (loop == 'S' || loop == 's');
