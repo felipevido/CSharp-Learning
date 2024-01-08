53f273606d960b0c7468a8e89310b33628fc10c3
Felipe Vido <contatofelipevido@gmail.com>
2024-01-08 19:23:46 -0300

    Exercícios While
    Estrutura de Repetição While

---
+++ ExercíciosSeção3/ExerciciosWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+Console.WriteLine("Informe sua senha:");
+string senha = Console.ReadLine();
+int tentativas = 0;
+
+    while (senha != "2002" && tentativas <= 5)
+    {
+        tentativas++;
+        System.Console.WriteLine("Senha inválida! Tente novamente");
+        senha = Console.ReadLine();
+    }
+    
+    if (senha == "2002") {
+        System.Console.WriteLine("Acesso permitido");
+    } else {
+        System.Console.WriteLine("Tentativas Excedidas. Senha bloqueada.");
+    }
+
+
+
+
+
