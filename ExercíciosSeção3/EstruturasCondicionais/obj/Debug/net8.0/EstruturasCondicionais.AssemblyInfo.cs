43c224bd43a6ee802ea09b35a2f06bd56653508c
Felipe Vido <contatofelipevido@gmail.com>
2024-01-18 20:47:45 -0300

    Seção 4
    Reconstrução da seção 4

--- ExercíciosSeção3/EstruturasCondicionais/obj/Debug/net8.0/EstruturasCondicionais.AssemblyInfo.cs
+++ ExercíciosSeção3/EstruturasCondicionais/obj/Debug/net8.0/EstruturasCondicionais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstruturasCondicionais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d546ba80786745e28558146c9658a30b3c39ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d155b9ff9f23f650fd72d1539bf3f68945dedd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstruturasCondicionais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstruturasCondicionais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
