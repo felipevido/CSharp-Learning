9c349fb7ca3c774efbf9f994a78664342224c90c
Felipe Vido <contatofelipevido@gmail.com>
2024-01-18 22:27:20 -0300

    Classes
    Criando Classes

---
+++ Seção 4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Seção_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Usuarios userA, userB; 
+
+        userA = new Usuarios();
+        userB = new Usuarios();
+
+        System.Console.WriteLine("TESTE DE CLASSES");
+        userA.Nome = Console.ReadLine();
+        userA.Idade = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(userA.Nome);
+        System.Console.WriteLine(userA.Idade);
+
+    }
+}
---
+++ Seção 4/Usuarios.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seção_4
+{
+    public class Usuarios
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
